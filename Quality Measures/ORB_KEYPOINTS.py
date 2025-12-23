715bda70ad521e1087f064265ec15f8c456affe0
Krzy-888 <kmacz.888@gmail.com>
2025-12-23 23:59:05 +0100

    Changing way of displaying points

--- Quality Measures/ORB_KEYPOINTS.py
+++ Quality Measures/ORB_KEYPOINTS.py
@@ -1,23 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-ORB = cv2.ORB_create()
+ORB = cv2.ORB_create(nfeatures = 800000)
 #draw Keypoints
-def draw_cross_keypoints(img, keypoints, color):
-    """ Draw keypoints as crosses, and return the new image with the crosses. """
-    img_kp = img.copy()  # Create a copy of img
 
-    # Iterate over all keypoints and draw a cross on evey point.
-    for kp in keypoints:
-        x, y = kp.pt  # Each keypoint as an x, y tuple  https://stackoverflow.com/questions/35884409/how-to-extract-x-y-coordinates-from-opencv-cv2-keypoint-object
-
-        x = int(round(x))  # Round an cast to int
-        y = int(round(y))
-
-        # Draw a cross with (x, y) center
-        cv2.drawMarker(img_kp, (x, y), color, markerType=cv2.MARKER_DIAMOND, markerSize=5, thickness=1, line_type=cv2.LINE_8)
-
-    return img_kp  # Return the image with the drawn crosses.
 paths = [r"Norm/CAPELLA_C05_URRC_Subset1000_2_gray.png",r"Norm/CAPELLA_C05_URRC_Subset1000_2_log.png",r"Norm/CAPELLA_C05_URRC_Subset1000_2_bad.png"]
 name = ["normalizacja 2-98%","min-max skala dB","normalizacja min-max"]
 fig, axes = plt.subplots(3, 1)
@@ -26,9 +12,11 @@ for i,path in enumerate(paths):
     img = cv2.imread(path,0)
     img_color = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
     kp, des = ORB.detectAndCompute(img, None)
-    img_2 = draw_cross_keypoints(img_color, kp, (0,255,0))
+    points = np.array([p.pt for p in kp])
     title = name[i] + f" Znaleziono: {len(kp)}"
-    axes[i].imshow(img_2)
+    print(len(kp))
+    axes[i].scatter(points[:,0],points[:,1],s=10)
+    axes[i].imshow(img,cmap="gray")
     axes[i].set_title(title)
 
 plt.tight_layout()
