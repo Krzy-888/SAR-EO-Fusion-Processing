93feff634bd5856029e4df323dd85f6817487276
Krzy-888 <kmacz.888@gmail.com>
2025-12-24 02:28:38 +0100

    Adding new versions of files

---
+++ Quality Measures/ORB_KEYPOINTS_2 copy.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+ORB = cv2.ORB_create(nfeatures = 800000)
+#draw Keypoints
+
+paths = [r"Norm/CAPELLA_C05_URRC_Subset1000_2_gray.png",r"Norm/CAPELLA_C05_URRC_Subset1000_2_log.png",r"Norm/CAPELLA_C05_URRC_Subset1000_2_bad.png"]
+name = ["2-98%","min-max skala dB","min-max"]
+fig, axes = plt.subplots(3, 1)
+fig.suptitle("ORB")
+for i,path in enumerate(paths):
+    title = name[i] + f" Z"
+    img = cv2.imread(path,0)
+    img_color = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    kp, des = ORB.detectAndCompute(img, None)
+    points = np.array([p.pt for p in kp])
+    title = name[i] + f" Znaleziono: {len(kp)}"
+    print(len(kp))
+    axes[i].scatter(points[:,0],points[:,1],s=10)
+    axes[i].imshow(img,cmap="gray")
+    axes[i].set_ylabel(name[i])
+
+plt.tight_layout()
+plt.show()
