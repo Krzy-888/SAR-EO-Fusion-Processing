32095bea75b2ecf58eddc1a4758d81bffe05b233
Krzy-888 <kmacz.888@gmail.com>
2026-01-06 01:18:56 +0100

    Adding Some Staffs

---
+++ TIFF2PNG_SUBSET_SAR_LOG_Resample.py
@@ -0,0 +1,116 @@
+#   LIBRARIES
+import rasterio
+from rasterio.enums import Resampling
+import numpy as np
+import cv2
+
+# Resampling https://rasterio.readthedocs.io/en/stable/topics/resampling.html
+def resample(path, factor):
+    '''
+        Docstring for resample
+
+        :param dataset: RasterioIMG
+        :param factor: Factor
+    '''
+    with rasterio.open(path) as dataset:
+        # resample data to target shape
+        data = dataset.read(
+            out_shape=(
+                dataset.count,
+                int(dataset.height * factor),
+                int(dataset.width * factor)
+            ),
+            resampling=Resampling.bilinear
+        )
+    return data
+
+#   NORMALIZE
+def normalize_to_uint8(array):
+    array = np.nan_to_num(array, nan=0.0, posinf=0.0, neginf=0.0)
+    array = array.astype(np.float32)
+    
+    # Logarytmic scale Db
+    if len(array) ==1:
+            log_array = 20*np.log10(array)
+            min_val = log_array.min()
+            max_val = log_array.max()
+            log_array = (log_array - min_val) / (max_val - min_val)
+            log_array *= 255 
+    
+    
+    for i in range(len(array)):
+        #clasic Normalization for EO
+        if len(img) == 3:
+            min_val = array[i].min()
+            max_val = array[i].max()
+        else:
+            #that pice of shit that may be helpful For SAR
+            min_val = array[i].min()
+            max_val = array[i].max()
+            bad_norm = (array - min_val) / (max_val - min_val)
+            bad_norm *= 255
+            min_val, max_val = np.percentile(array, (2, 98))
+            array = np.clip(array, min_val, max_val)
+        array[i] = (array[i] - min_val) / (max_val - min_val)      
+
+    array *= 255
+    if len(array) == 1:
+        return array.astype(np.uint8), log_array.astype(np.uint8),bad_norm.astype(np.uint8)
+    else:
+         return array.astype(np.uint8)
+
+#   DATASET
+paths = [r"ORG_Data/CAPELLA_C05_URRC_SUB.tif",r"ORG_Data/CAPELLA_C06_UIAA_SUB.tif",r"ORG_Data/CAPELLA_C09_URWH_SUB.tif",r"ORG_Data/CAPELLA_C11_UDYE_SUB.tif",r"ORG_Data/IMG_PNEO3_UDYE_SUB.tif",r"ORG_Data/IMG_PNEO4_UIAA_SUB.tif",r"ORG_Data/IMG_PNEO4_URRC_SUB.tif",r"ORG_Data/IMG_PNEO4_URWH_SUB.tif"]
+names = [10,1,"035"]
+factors = [0.35/10,0.35/1,1]
+#   resample AND PROCESS
+
+for i,path in enumerate(paths):
+    name = path.split("\\")[-1].replace(".tif","")
+    with rasterio.open(path) as src:
+        for j,f in enumerate(factors):
+            img = resample(path,f)
+
+            if len(img) == 3:
+                norm = normalize_to_uint8(img)
+            else:
+                norm,log,bnorm = normalize_to_uint8(img)
+            name2 = f"{name}_{names[j]}m"
+            print(name2)
+            print("\t***Before***")
+            print(img.shape)
+            print(img.max())
+            print(img.min())
+            print("\t***After***")
+            print(norm.shape)
+            print(norm.max())
+            print(norm.min())
+            if len(img) == 1:
+                print("\t***Log***\t")
+                print(log.shape)
+                print(log.max())
+                print(log.min())
+                print("\t***bad***\t")
+                print(bnorm.shape)
+                print(bnorm.max())
+                print(bnorm.min())
+            if len(img) == 3:
+                blue = norm[2]
+                green = norm[1]
+                red = norm[0]
+                bgr = np.stack([blue, green, red], axis=-1)
+                norm_2 = cv2.cvtColor(bgr,cv2.COLOR_BGR2GRAY)
+                out_g = f"Norm\\{name2}_gray.png"
+                cv2.imwrite(out_g,norm_2)
+            else:
+                gray = norm.mean(axis=0).astype(np.uint8)
+                gray_l = log.mean(axis=0).astype(np.uint8)
+                gray_b = bnorm.mean(axis=0).astype(np.uint8)
+                out_g = f"Norm\\{name2}_gray.png"
+                out_l = f"Norm\\{name2}_log.png"
+                out_b = f"Norm\\{name2}_bad.png"
+                cv2.imwrite(out_g,gray)
+                cv2.imwrite(out_l,gray_l)
+                cv2.imwrite(out_b,gray_b)
+# SAVE
+print("DONE")
--- mach_points_plot_lib.py
+++ mach_points_plot_lib.py
@@ -4,6 +4,13 @@ import cv2
 
 
 def mach_imgpoints(img1,img2,points2):
+    """
+    Docstring for mach_imgpoints
+    
+    :param img1: Description
+    :param img2: Description
+    :param points2: Description
+    """
     col_num = img1.shape[1] + img2.shape[1]
     row_num = max(img1.shape[0], img2.shape[0])
     new_img = np.zeros((row_num, col_num), dtype=np.uint8)
@@ -14,9 +21,19 @@ def mach_imgpoints(img1,img2,points2):
     return new_img,new_points
 
 def show_maches_in_axis(axis,img1,img2,points1,points2,colors):
+    """
+    Docstring for show_maches_in_axis
+
+    :param axis: Description
+    :param img1: Description
+    :param img2: Description
+    :param points1: Description
+    :param points2: Description
+    :param colors: Description
+    """
     img,points_new = mach_imgpoints(img1,img2,points2)
     axis.imshow(img,cmap="gray")
-    for i in range(len(ptk_PNEO)):
+    for i in range(len(points2)):
         if len(colors)>3:
             color = colors[i]
         else:
@@ -24,3 +41,20 @@ def show_maches_in_axis(axis,img1,img2,points1,points2,colors):
         axis.plot([points1[i,0],points_new[i,0]],[points1[i,1],points_new[i,1]],c=color)
         axis.scatter(points1[i,0],points1[i,1],c=color)
         axis.scatter(points_new[i,0],points_new[i,1],c=color)
+
+def resamplepoints(data,default_grd,scales):
+    """
+    Docstring for przeskaluj_pkt
+
+    :param data: Description
+    :param default_grd: Description
+    :param scales: Description
+    """
+    data_new = []
+    for scale in scales:
+        denominator = scale/default_grd
+        data_new.append(data/denominator)
+    return data_new
+
+def show_points(ax,points):
+    ax.scatter(points[0],points[1])
