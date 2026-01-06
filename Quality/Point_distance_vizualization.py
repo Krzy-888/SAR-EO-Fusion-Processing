dd9e2365bb87913b1c7771d0b8705ea8d336ab9b
Krzy-888 <kmacz.888@gmail.com>
2026-01-06 20:20:27 +0100

    Adding points and descriptors data

--- Quality/Point_distance_vizualization.py
+++ Quality/Point_distance_vizualization.py
@@ -6,18 +6,18 @@ import matplotlib.colors as mc
 import RMSE
 
 # DANE
-ptk_PNEO = np.genfromtxt(r"Norm/UTM_URRC_PNEO.csv", delimiter=',',dtype=np.float32)
+ptk_PNEO = np.genfromtxt(r"RefPoints/UTM_URRC_PNEO.csv", delimiter=',',dtype=np.float32)
 print(ptk_PNEO)
-ptk_CAPELLA = np.genfromtxt(r"Norm/UTM_URRC_CAPELLA.csv", delimiter=',',dtype=np.float32)
+ptk_CAPELLA = np.genfromtxt(r"RefPoints/UTM_URRC_CAPELLA.csv", delimiter=',',dtype=np.float32)
 print(ptk_CAPELLA)
-img1 = cv2.imread(r"Norm/CAPELLA_C05_URRC_Subset1000_2_gray.png",0)
-img2 = cv2.imread(r"Norm/IMG_PNEO4_URRC_Subset1000_gray.png",0)
+img1 = cv2.imread(r"Norm/SAR_URRC_SUB_035m_gray.png",0)
+img2 = cv2.imread(r"Norm/EO_URRC_SUB_035m_gray.png",0)
 
 # RMSE i Transformacja
 M,mask =cv2.estimateAffine2D(ptk_CAPELLA,ptk_PNEO)
 rmse,blad = RMSE.calculate_RMSE(M,ptk_CAPELLA,ptk_PNEO)
-print(blad)
-print(rmse)
+print(blad*0.35)
+print(rmse*0.35)
 
 # Figure
 kolory = ["yellow","orange","red"]
@@ -25,4 +25,11 @@ kolor = mc.LinearSegmentedColormap.from_list('mycmap', kolory)
 plt.scatter(ptk_PNEO[:,0],ptk_PNEO[:,1], c=blad,cmap=kolor)
 plt.colorbar()
 plt.imshow(img2,cmap="gray")
+plt.show()
+#TEST
+h,w = img2.shape
+image = cv2.warpAffine(img1,M,(h,w))
+plt.imshow(image,cmap="gray")
+plt.show()
+plt.imshow(img1,cmap="gray")
 plt.show()
---
+++ Quality/Point_distance_vizualization_multi_scale.py
@@ -0,0 +1,75 @@
+# Biblioteki
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.colors as mc
+import RMSE
+
+# DANE
+ptk_PNEO = np.genfromtxt(r"RefPoints/UTM_UIAA_PNEO.csv", delimiter=',',dtype=np.float32)
+print(ptk_PNEO)
+ptk_CAPELLA = np.genfromtxt(r"RefPoints/UTM_UIAA_CAPELLA.csv", delimiter=',',dtype=np.float32)
+print(ptk_CAPELLA)
+paths2 = [r"Norm/EO_UIAA_SUB_10m_gray.png",r"Norm/EO_UIAA_SUB_1m_gray.png",r"Norm/EO_UIAA_SUB_035m_gray.png"]
+kolory = ["yellow","orange","red"]
+kolor = mc.LinearSegmentedColormap.from_list('mycmap', kolory)
+vmin = 0
+vmax = 10
+blad_min = []
+blad_max = []
+pkt_PNEO_list = []
+pkt_CAPELLA_list = []
+new_blad_list = []
+grd = [10,1,0.35]
+ilorazy = [grd[0]/0.35,grd[1]/0.35,grd[2]/0.35]
+rmse_list = []
+matrix_list =[]
+for i,path in enumerate(paths2):
+    n_pkt_neo = ptk_PNEO/ilorazy[i]
+    #print(n_pkt_neo)
+    pkt_PNEO_list.append(n_pkt_neo)
+    n_pkt_cap = ptk_CAPELLA/ilorazy[i]
+    #print(n_pkt_cap)
+    pkt_CAPELLA_list.append(n_pkt_cap)
+    M,mask =cv2.estimateAffine2D(n_pkt_cap,n_pkt_neo)
+    rmse,blad = RMSE.calculate_RMSE(M,n_pkt_cap,n_pkt_neo)
+    #print(M)
+    blad_2 = np.array(blad)*grd[i]
+    blad_min.append(min(blad_2))
+    blad_max.append(max(blad_2))
+    new_blad_list.append(blad_2)
+    rmse_list.append(rmse*grd[i])
+    matrix_list.append(M)
+vmin = min(blad_min)
+vmax = max(blad_max)
+fig, axes = plt.subplots(3, 1,constrained_layout=True)
+
+#axes[0].set_title("SAR")
+for i,path in enumerate(paths2):
+    pkt = pkt_PNEO_list[i]
+    img = cv2.imread(path,0)
+    sc = axes[i].scatter(pkt[:,0],pkt[:,1], c=new_blad_list[i],cmap=kolor,vmin=vmin,vmax=vmax)
+    axes[i].imshow(img,cmap="gray")
+    
+    print(rmse_list[i])
+    print(new_blad_list[i])
+
+fig.colorbar(sc, ax=axes, orientation="vertical")
+#plt.tight_layout()
+plt.show()
+
+# Visual Test
+transform_list = []
+for i,path in enumerate(paths2):
+    img_EO = cv2.imread(path,0)
+    img_SAR = cv2.imread(path.replace("EO_","SAR_"),0)
+    h,w = img_EO.shape
+    image = cv2.warpAffine(img_SAR,matrix_list[i],(h,w))
+    transform_list.append(image)
+
+
+fig2, axes2 = plt.subplots(3, 1)
+for i,img in enumerate(transform_list):
+    axes2[i].imshow(img,cmap="gray")
+plt.tight_layout()
+plt.show()
