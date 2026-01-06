246ae6b7ecd068dc86b963e30fd8e5d165ade9cd
Krzy-888 <kmacz.888@gmail.com>
2026-01-06 14:52:12 +0100

    Adding Quality Library

--- Quality/RMSE.py
+++ Quality/RMSE.py
@@ -20,5 +20,23 @@ def calculate_RMSE(Macierz,pkt_zrod,pkt_ref):
     # Odleglosc
     odleglosc = np.sum(roznica**2, axis=1)
     # RMSE axis=1 zapewnia 
-    RMSE = np.sqrt(np.mean(odleglosc))
+    RMSE = np.sqrt(sum(odleglosc)/(len(odleglosc)-1))
     return RMSE, odleglosc
+
+def calculate_CMR(pkt_ref1,pkt_ref2,src_pts,dst_pts,treshold):
+    """
+    Docstring for calculate_CMR
+    
+    :param pkt_ref1: Description
+    :param pkt_ref2: Description
+    :param src_pts: Description
+    :param dst_pts: Description
+    :param treshold: Description
+    """
+    
+    M,_ = cv2.estimateAffine2D(pkt_ref1,pkt_ref2)
+    rmse,blad = calculate_RMSE(M,src_pts,dst_pts)
+    corr = blad[blad <= treshold]
+    CMR = len(corr)/len(blad)*100
+    return CMR,rmse,blad
+
