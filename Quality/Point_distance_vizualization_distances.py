79c693bc62b4b3e46e1f5ead0e06aee37f19d5d5
Krzy-888 <kmacz.888@gmail.com>
2026-01-07 02:07:46 +0100

    New measurments and poinst visualization

--- Quality/Point_distance_vizualization_distances.py
+++ Quality/Point_distance_vizualization_distances.py
@@ -12,8 +12,12 @@ ptk_CAPELLA = np.genfromtxt(r"RefPoints/UTM_URWH_CAPELLA.csv", delimiter=',',dty
 print(ptk_CAPELLA)
 img1 = cv2.imread(r"Norm/SAR_URWH_SUB_035m_gray.png",0)
 img2 = cv2.imread(r"Norm/EO_URWH_SUB_035m_gray.png",0)
-
+diff = ptk_PNEO-ptk_CAPELLA
 # RMSE i Transformacja
+print(diff)
+dist = np.linalg.norm(diff, axis=1)
+print(dist)
+print(np.mean(dist))
 M,mask =cv2.estimateAffine2D(ptk_CAPELLA,ptk_PNEO)
 rmse,blad = RMSE.calculate_RMSE(M,ptk_CAPELLA,ptk_PNEO)
 print(blad*0.35)
@@ -27,9 +31,9 @@ plt.colorbar()
 plt.imshow(img2,cmap="gray")
 plt.show()
 #TEST
-#h,w = img2.shape
-#image = cv2.warpAffine(img1,M,(h,w))
-#plt.imshow(image,cmap="gray")
-#plt.show()
+h,w = img2.shape
+image = cv2.warpAffine(img1,M,(h,w))
+plt.imshow(image,cmap="gray")
+plt.show()
 #plt.imshow(img1,cmap="gray")
 #plt.show()
